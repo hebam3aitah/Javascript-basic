bb53847a9d8e286a105168721b5c415ee620907d
hebam3aitah <hebam3aitah12@gmail.com>
2024-12-26 16:51:08 +0300

    task files

---
+++ task.js
@@ -0,0 +1,32 @@
+let x=5;
+console.log(5*8);
+console.log("javascript"+5);
+console.log(17 % 5);
+console.log(5 % 17);
+console.log(5/10);
+console.log(4 === "4");
+console.log(4 != 5);
+console.log(7<=8 );
+console.log("Hello" + 5 );
+console.log(Math.ceil(x)-Math.floor(x));
+console.log(Math.pow(x,2));
+
+let num=prompt("Q2:plz Enter the num:");
+window.alert( num);
+window.alert( "Q3:the sum =");
+let num1=parseInt(prompt("plz Enter the num1:"));
+let num2=parseInt(prompt("plz Enter the num2:"));
+window.alert( "the sum ="+ (num1 +num2));
+
+window.alert( "Q4:the dividing =");
+let num3=prompt("plz Enter the num1:");
+let num4=prompt("plz Enter the num2:");
+window.alert( "the dividing ="+ num3 /num4);
+
+window.alert("Q5:the product");
+let num5=prompt("plz Enter the num1:");
+let num6=prompt("plz Enter the num2:");
+window.alert( "the product ="+ num5 *num6);
+
+window.alert( "Q6: window.alert");
+window.alert ( "the sum 5 + 8 =" + (5 + 8));
